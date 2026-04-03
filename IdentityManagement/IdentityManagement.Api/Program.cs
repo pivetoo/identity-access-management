6b856783993b37c603d3eee77431f43eba9fb22c
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2026-04-03 00:40:13 -0300

    feat: adiciona config archon

--- IdentityManagement/IdentityManagement.Api/Program.cs
+++ IdentityManagement/IdentityManagement.Api/Program.cs
@@ -1,23 +1,34 @@
+using Archon.Api.DependencyInjection;
+using Archon.Api.MultiTenancy;
+using Archon.Infrastructure.DependencyInjection;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
+builder.Services.AddArchonApi(builder.Configuration);
+builder.Services.AddArchonPersistence(builder.Configuration, typeof(Program).Assembly);
+builder.Services.AddArchonAuthentication(builder.Configuration);
+builder.Services.AddServicesFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
+app.UseArchonApi();
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSessionValidation();
+app.UseIdentityManagementUserSync();
 
 app.MapControllers();
+
+await app.UseArchonAccessSyncAsync();
 
 app.Run();
