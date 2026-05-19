5cdc66cbba6d1517a2619ee27e84365e5ca8f023
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2026-05-19 16:55:18 -0300

    refactor: atualiza testes do AccessResource para incluir parametro area

--- IdentityManagement/IdentityManagement.Testing/Domain/Entities/AccessResourceTests.cs
+++ IdentityManagement/IdentityManagement.Testing/Domain/Entities/AccessResourceTests.cs
@@ -8,11 +8,12 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Constructor_WithValidParameters_ShouldCreateAccessResource()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
             Assert.That(resource.SystemApplicationId, Is.EqualTo(1));
             Assert.That(resource.Name, Is.EqualTo("Users.Read"));
             Assert.That(resource.Description, Is.EqualTo("Read users"));
+            Assert.That(resource.Area, Is.EqualTo("Users"));
             Assert.That(resource.Controller, Is.EqualTo("UsersController"));
             Assert.That(resource.Action, Is.EqualTo("Get"));
             Assert.That(resource.HttpMethod, Is.EqualTo("GET"));
@@ -24,87 +25,102 @@ namespace IdentityManagement.Testing.Domain.Entities
         [TestCase(-1)]
         public void Constructor_WithInvalidSystemApplicationId_ShouldThrowArgumentOutOfRangeException(long systemApplicationId)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new AccessResource(systemApplicationId, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AccessResource(systemApplicationId, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users"));
         }
 
         [Test]
         public void Constructor_WithNullName_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, null!, "Read users", "UsersController", "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, null!, "Read users", "Users", "UsersController", "Get", "GET", "/api/users"));
         }
 
         [TestCase("")]
         [TestCase("   ")]
         public void Constructor_WithInvalidName_ShouldThrowArgumentException(string name)
         {
-            Assert.Throws<ArgumentException>(() => new AccessResource(1, name, "Read users", "UsersController", "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentException>(() => new AccessResource(1, name, "Read users", "Users", "UsersController", "Get", "GET", "/api/users"));
         }
 
         [Test]
         public void Constructor_WithNullDescription_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", null!, "UsersController", "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", null!, "Users", "UsersController", "Get", "GET", "/api/users"));
+        }
+
+        [Test]
+        public void Constructor_WithNullArea_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", null!, "UsersController", "Get", "GET", "/api/users"));
+        }
+
+        [Test]
+        public void Constructor_WithEmptyArea_ShouldCreateAccessResource()
+        {
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", string.Empty, "UsersController", "Get", "GET", "/api/users");
+
+            Assert.That(resource.Area, Is.EqualTo(string.Empty));
         }
 
         [Test]
         public void Constructor_WithNullController_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", null!, "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", null!, "Get", "GET", "/api/users"));
         }
 
         [TestCase("")]
         [TestCase("   ")]
         public void Constructor_WithInvalidController_ShouldThrowArgumentException(string controller)
         {
-            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", controller, "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", controller, "Get", "GET", "/api/users"));
         }
 
         [Test]
         public void Constructor_WithNullAction_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", null!, "GET", "/api/users"));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", null!, "GET", "/api/users"));
         }
 
         [TestCase("")]
         [TestCase("   ")]
         public void Constructor_WithInvalidAction_ShouldThrowArgumentException(string action)
         {
-            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", action, "GET", "/api/users"));
+            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", action, "GET", "/api/users"));
         }
 
         [Test]
         public void Constructor_WithNullHttpMethod_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", null!, "/api/users"));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", null!, "/api/users"));
         }
 
         [TestCase("")]
         [TestCase("   ")]
         public void Constructor_WithInvalidHttpMethod_ShouldThrowArgumentException(string httpMethod)
         {
-            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", httpMethod, "/api/users"));
+            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", httpMethod, "/api/users"));
         }
 
         [Test]
         public void Constructor_WithNullRoute_ShouldThrowArgumentNullException()
         {
-            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", null!));
+            Assert.Throws<ArgumentNullException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", null!));
         }
 
         [TestCase("")]
         [TestCase("   ")]
         public void Constructor_WithInvalidRoute_ShouldThrowArgumentException(string route)
         {
-            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", route));
+            Assert.Throws<ArgumentException>(() => new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", route));
         }
 
         [Test]
         public void Constructor_ShouldTrimInputValues()
         {
-            AccessResource resource = new AccessResource(1, "  Users.Read  ", "  Read users  ", "  UsersController  ", "  Get  ", "  get  ", "  /api/users  ");
+            AccessResource resource = new AccessResource(1, "  Users.Read  ", "  Read users  ", "  Users  ", "  UsersController  ", "  Get  ", "  get  ", "  /api/users  ");
 
             Assert.That(resource.Name, Is.EqualTo("Users.Read"));
             Assert.That(resource.Description, Is.EqualTo("Read users"));
+            Assert.That(resource.Area, Is.EqualTo("Users"));
             Assert.That(resource.Controller, Is.EqualTo("UsersController"));
             Assert.That(resource.Action, Is.EqualTo("Get"));
             Assert.That(resource.HttpMethod, Is.EqualTo("GET"));
@@ -114,7 +130,7 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Constructor_ShouldConvertHttpMethodToUpperCase()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "get", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "get", "/api/users");
 
             Assert.That(resource.HttpMethod, Is.EqualTo("GET"));
         }
@@ -122,12 +138,13 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Update_WithValidParameters_ShouldUpdateProperties()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
-            resource.Update(2, "Read all users", "UsersController", "GetAll", "POST", "/api/users/all");
+            resource.Update(2, "Read all users", "UsersAdmin", "UsersController", "GetAll", "POST", "/api/users/all");
 
             Assert.That(resource.SystemApplicationId, Is.EqualTo(2));
             Assert.That(resource.Description, Is.EqualTo("Read all users"));
+            Assert.That(resource.Area, Is.EqualTo("UsersAdmin"));
             Assert.That(resource.Controller, Is.EqualTo("UsersController"));
             Assert.That(resource.Action, Is.EqualTo("GetAll"));
             Assert.That(resource.HttpMethod, Is.EqualTo("POST"));
@@ -137,26 +154,34 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Update_ShouldConvertHttpMethodToUpperCase()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
-            resource.Update(1, "Read users", "UsersController", "Get", "post", "/api/users");
+            resource.Update(1, "Read users", "Users", "UsersController", "Get", "post", "/api/users");
 
             Assert.That(resource.HttpMethod, Is.EqualTo("POST"));
+        }
+
+        [Test]
+        public void Update_WithNullArea_ShouldThrowArgumentNullException()
+        {
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
+
+            Assert.Throws<ArgumentNullException>(() => resource.Update(1, "Read users", null!, "UsersController", "Get", "GET", "/api/users"));
         }
 
         [TestCase(0)]
         [TestCase(-1)]
         public void Update_WithInvalidSystemApplicationId_ShouldThrowArgumentOutOfRangeException(long systemApplicationId)
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
-            Assert.Throws<ArgumentOutOfRangeException>(() => resource.Update(systemApplicationId, "Read users", "UsersController", "Get", "GET", "/api/users"));
+            Assert.Throws<ArgumentOutOfRangeException>(() => resource.Update(systemApplicationId, "Read users", "Users", "UsersController", "Get", "GET", "/api/users"));
         }
 
         [Test]
         public void Activate_ShouldSetIsActiveToTrue()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
             resource.Deactivate();
 
             resource.Activate();
@@ -167,7 +192,7 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Deactivate_ShouldSetIsActiveToFalse()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
             resource.Deactivate();
 
@@ -177,7 +202,7 @@ namespace IdentityManagement.Testing.Domain.Entities
         [Test]
         public void Collections_ShouldBeEmptyOnCreation()
         {
-            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "UsersController", "Get", "GET", "/api/users");
+            AccessResource resource = new AccessResource(1, "Users.Read", "Read users", "Users", "UsersController", "Get", "GET", "/api/users");
 
             Assert.That(resource.RoleAccessResources, Is.Empty);
         }
