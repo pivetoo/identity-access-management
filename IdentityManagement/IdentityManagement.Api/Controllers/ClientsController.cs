23a9bbba422d8332cd72486436098f1f79f0c5cc
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2026-06-04 22:41:17 -0300

    feat: endpoint POST clients/onboard

---
+++ IdentityManagement/IdentityManagement.Api/Controllers/ClientsController.cs
@@ -0,0 +1,30 @@
+using Archon.Api.Attributes;
+using Archon.Api.Controllers;
+using IdentityManagement.Application.Requests.Clients;
+using IdentityManagement.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityManagement.Api.Controllers
+{
+    public sealed class ClientsController : ApiControllerBase
+    {
+        private readonly IClientOnboardingService clientOnboardingService;
+        private readonly IConfiguration configuration;
+
+        public ClientsController(IClientOnboardingService clientOnboardingService, IConfiguration configuration)
+        {
+            this.clientOnboardingService = clientOnboardingService;
+            this.configuration = configuration;
+        }
+
+        [RequireAccess]
+        [PostEndpoint("onboard")]
+        public async Task<IActionResult> Onboard([FromBody] OnboardClientRequest request, CancellationToken cancellationToken)
+        {
+            string setupBaseUrl = configuration["Oidc:Issuer"] ?? string.Empty;
+            var response = await clientOnboardingService.OnboardClient(request, setupBaseUrl, cancellationToken);
+            return Http200(response);
+        }
+    }
+}
